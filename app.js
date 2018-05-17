71ecc748be3eb292cbac4fd720d9860b43f42c10
Ricardo Araujo <ricardo-araujo@forseti.com.br>
2018-05-16 21:38:16 -0300

    Iniciando conexao com bd

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-var express = require('express');
-var app = express();
-
-app.get('/produtos', function(req, res) {
-	res.send('<html><body><h1>Listagem de Produtos</h1></body></html>');
-});
-
+var app = require('./config/express')();
+var produtosRoute =  require('./app/routes/produtos')(app);
 
 app.listen(8000, function() {
 	console.log('Server is running...');
