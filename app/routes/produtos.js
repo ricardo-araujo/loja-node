3010cb180eb11ba9b53944c94ae0d3912c9bf146
Ricardo Araujo <ricardo-araujo@forseti.com.br>
2018-05-23 21:42:13 -0300

    Post de produtos e validacao de inputs

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -14,13 +14,24 @@ module.exports = function(app) {
 
     app.get('/produtos/form', function(req, res) {
 
-        res.render('produtos/form');
+        res.render('produtos/form', {validationErrors: {}, produto: {}});
 
     });
 
     app.post('/produtos', function(req, res) {
 
         var produto = req.body;
+
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('preco', 'Formato inválido').isFloat();
+
+        var errors = req.validationErrors();
+
+        if (errors) {
+            res.render('produtos/form', {validationErrors: errors, produto: produto});
+            return;
+        }
+
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
 
--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 module.exports = function() {
     var app = express();
@@ -9,6 +10,8 @@ module.exports = function() {
     app.set('views', './app/views');
 
     app.use(bodyParser.urlencoded({extended:true}));
+    //app.use(bodyParser.json());
+    app.use(expressValidator());
 
     load('routes', {cwd: 'app'})
         .then('infra')
